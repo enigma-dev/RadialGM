c4a361fbaeb7bba98d6aa4473148f9e0ba9edd19
Greg Williamson <cheeseboy16@gmail.com>
2018-03-21 15:18:07 +0000

    bkg fixes

--- Widgets/BackgroundRenderer.cpp
+++ Widgets/BackgroundRenderer.cpp
@@ -20,7 +20,7 @@ void BackgroundRenderer::setImage(QPixmap image) {
   }
 
   transparentPixmap = QPixmap::fromImage(img);
-  setFixedSize(pixmap.width(), pixmap.height());
+  setFixedSize(pixmap.width() + 1, pixmap.height() + 1);
   update();
 }
 
@@ -43,27 +43,37 @@ void BackgroundRenderer::setZoom(qreal zoom) {
   this->zoom = zoom;
   if (this->zoom > 3200) this->zoom = 3200;
   if (this->zoom < 0.0625) this->zoom = 0.0625;
-  setFixedSize(static_cast<int>(pixmap.width() * this->zoom), static_cast<int>(pixmap.height() * this->zoom));
+  setFixedSize(static_cast<int>(pixmap.width() * this->zoom) + 1, static_cast<int>(pixmap.height() * this->zoom) + 1);
 }
 
 const qreal &BackgroundRenderer::getZoom() const { return zoom; }
 
 void BackgroundRenderer::paintEvent(QPaintEvent * /* event */) {
   QPainter painter(this);
+
   painter.fillRect(QRectF(0, 0, pixmap.width() * zoom, pixmap.height() * zoom), ArtManager::get_transpareny_brush());
 
   painter.scale(zoom, zoom);
-
   painter.drawPixmap(0, 0, (transparent) ? transparentPixmap : pixmap);
 
   if (grid_visible) {
     painter.setCompositionMode(QPainter::RasterOp_SourceXorDestination);
     painter.setPen(QColor(0xff, 0xff, 0xff));
 
-    for (int x = grid_hOff; x < pixmap.width(); x += grid_w + grid_hSpacing) {
-      for (int y = grid_vOff; y < pixmap.height(); y += grid_h + grid_vSpacing) {
-        painter.drawRect(x, y, grid_w - 1, grid_h - 1);
+    if (grid_hSpacing != 0 || grid_vSpacing != 0) {
+      for (int x = grid_hOff; x < pixmap.width(); x += grid_w + grid_hSpacing) {
+        for (int y = grid_vOff; y < pixmap.height(); y += grid_h + grid_vSpacing) {
+          painter.drawRect(x, y, grid_w, grid_h);
+        }
       }
+    }
+
+    if (grid_hSpacing == 0) {
+      for (int x = grid_hOff; x <= pixmap.width(); x += grid_w) painter.drawLine(x, 0, x, pixmap.height());
+    }
+
+    if (grid_vSpacing == 0) {
+      for (int y = grid_vOff; y <= pixmap.width(); y += grid_h) painter.drawLine(0, y, pixmap.width(), y);
     }
   }
 }
