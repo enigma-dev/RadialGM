2371299d27b7542653f8535b93ae90a110337621
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-03-25 06:22:46 -0400

    refactor protomodel using google

--- Models/ProtoModel.cpp
+++ Models/ProtoModel.cpp
@@ -1,12 +1,15 @@
 #include "ProtoModel.h"
 
-ProtoModel::ProtoModel(google::protobuf::Message *protobuf, QObject *parent)
+using namespace google::protobuf;
+using CppType = FieldDescriptor::CppType;
+
+ProtoModel::ProtoModel(Message *protobuf, QObject *parent)
     : QAbstractItemModel(parent), dirty(false), protobuf(protobuf) {
   protobufBackup = protobuf->New();
   protobufBackup->CopyFrom(*protobuf);
 }
 
-void ProtoModel::ReplaceBuffer(google::protobuf::Message *buffer) {
+void ProtoModel::ReplaceBuffer(Message *buffer) {
   SetDirty(true);
   protobuf->CopyFrom(*buffer);
   emit dataChanged(index(0), index(rowCount()));
@@ -18,7 +21,7 @@ void ProtoModel::RestoreBuffer() {
 }
 
 int ProtoModel::rowCount(const QModelIndex & /*parent*/) const {
-  const google::protobuf::Descriptor *desc = protobuf->GetDescriptor();
+  const Descriptor *desc = protobuf->GetDescriptor();
   return desc->field_count();
 }
 
@@ -29,40 +32,40 @@ bool ProtoModel::IsDirty() { return dirty; }
 int ProtoModel::columnCount(const QModelIndex & /*parent*/) const { return 1; }
 
 bool ProtoModel::setData(const QModelIndex &index, const QVariant &value, int /*role*/) {
-  const google::protobuf::Descriptor *desc = protobuf->GetDescriptor();
-  const google::protobuf::Reflection *refl = protobuf->GetReflection();
-  const google::protobuf::FieldDescriptor *field = desc->FindFieldByNumber(index.row());
+  const Descriptor *desc = protobuf->GetDescriptor();
+  const Reflection *refl = protobuf->GetReflection();
+  const FieldDescriptor *field = desc->FindFieldByNumber(index.row());
   if (!field) return false;
 
   switch (field->cpp_type()) {
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_MESSAGE: {
+    case CppType::CPPTYPE_MESSAGE: {
       break;
     }
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_INT32:
+    case CppType::CPPTYPE_INT32:
       refl->SetInt32(protobuf, field, value.toInt());
       break;
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_INT64:
+    case CppType::CPPTYPE_INT64:
       refl->SetInt64(protobuf, field, value.toLongLong());
       break;
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_UINT32:
+    case CppType::CPPTYPE_UINT32:
       refl->SetUInt32(protobuf, field, value.toUInt());
       break;
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_UINT64:
+    case CppType::CPPTYPE_UINT64:
       refl->SetUInt64(protobuf, field, value.toULongLong());
       break;
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_DOUBLE:
+    case CppType::CPPTYPE_DOUBLE:
       refl->SetDouble(protobuf, field, value.toDouble());
       break;
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_FLOAT:
+    case CppType::CPPTYPE_FLOAT:
       refl->SetFloat(protobuf, field, value.toFloat());
       break;
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_BOOL:
+    case CppType::CPPTYPE_BOOL:
       refl->SetBool(protobuf, field, value.toBool());
       break;
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_ENUM:
+    case CppType::CPPTYPE_ENUM:
       refl->SetEnum(protobuf, field, field->enum_type()->FindValueByNumber(value.toInt()));
       break;
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_STRING:
+    case CppType::CPPTYPE_STRING:
       refl->SetString(protobuf, field, value.toString().toStdString());
       break;
   }
@@ -77,31 +80,31 @@ QVariant ProtoModel::data(int index) const { return data(this->index(index, 0, Q
 QVariant ProtoModel::data(const QModelIndex &index, int role) const {
   if (role != Qt::DisplayRole && role != Qt::EditRole) return QVariant();
 
-  const google::protobuf::Descriptor *desc = protobuf->GetDescriptor();
-  const google::protobuf::Reflection *refl = protobuf->GetReflection();
-  const google::protobuf::FieldDescriptor *field = desc->FindFieldByNumber(index.row());
+  const Descriptor *desc = protobuf->GetDescriptor();
+  const Reflection *refl = protobuf->GetReflection();
+  const FieldDescriptor *field = desc->FindFieldByNumber(index.row());
   if (!field) return QVariant();
 
   switch (field->cpp_type()) {
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_MESSAGE:
+    case CppType::CPPTYPE_MESSAGE:
       return refl->GetInt32(*protobuf, field);
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_INT32:
+    case CppType::CPPTYPE_INT32:
       return refl->GetInt32(*protobuf, field);
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_INT64:
+    case CppType::CPPTYPE_INT64:
       return static_cast<long long>(refl->GetInt64(*protobuf, field));
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_UINT32:
+    case CppType::CPPTYPE_UINT32:
       return refl->GetUInt32(*protobuf, field);
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_UINT64:
+    case CppType::CPPTYPE_UINT64:
       return static_cast<unsigned long long>(refl->GetUInt64(*protobuf, field));
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_DOUBLE:
+    case CppType::CPPTYPE_DOUBLE:
       return refl->GetDouble(*protobuf, field);
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_FLOAT:
+    case CppType::CPPTYPE_FLOAT:
       return refl->GetFloat(*protobuf, field);
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_BOOL:
+    case CppType::CPPTYPE_BOOL:
       return refl->GetBool(*protobuf, field);
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_ENUM:
+    case CppType::CPPTYPE_ENUM:
       return refl->GetInt32(*protobuf, field);
-    case google::protobuf::FieldDescriptor::CppType::CPPTYPE_STRING:
+    case CppType::CPPTYPE_STRING:
       return refl->GetString(*protobuf, field).c_str();
   }
 
