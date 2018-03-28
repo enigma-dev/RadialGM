aad1b5e3e5018e9da1286568cdcc650b6afecb40
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-03-27 21:37:02 -0400

    some comments to explain ImmediateMapper behavior

--- Models/ImmediateMapper.cpp
+++ Models/ImmediateMapper.cpp
@@ -7,6 +7,8 @@
 #include <QMetaProperty>
 
 ImmediateDataWidgetMapper::ImmediateDataWidgetMapper(QObject *parent) : QDataWidgetMapper(parent) {
+  // we do manual submission because auto submission will commit even on left click
+  // and give false-positives about the dirty state of our model
   this->setSubmitPolicy(QDataWidgetMapper::ManualSubmit);
 }
 
@@ -26,10 +28,18 @@ void ImmediateDataWidgetMapper::widgetChanged() {
 }
 
 void ImmediateDataWidgetMapper::addMapping(QWidget *widget, int section, QByteArray propertyName) {
+  // since QDataWidgetMapper makes its widget map private we have no way of
+  // getting all of the widgets registered with this mapper
+  // we also do not want to hook up a widget that's already mapped
   if (widgetList.contains(widget)) return;
   widgetList.append(widget);
   QDataWidgetMapper::addMapping(widget, section, propertyName);
+  // we use the QDataWidgetMapper to tell us what property we should listen to for changes
+  // all widgets have several properties, but most have a "primary" property with which
+  // the user interacts, i.e. "checked" for QCheckBox
   propertyName = this->mappedPropertyName(widget);
+  // we use the widget's meta object in order to get us a signal that we can listen to
+  // so we know when this property changes
   auto widgetMetaObject = widget->metaObject();
   auto property = widgetMetaObject->property(widgetMetaObject->indexOfProperty(propertyName));
   auto metaObject = this->metaObject();
@@ -37,11 +47,19 @@ void ImmediateDataWidgetMapper::addMapping(QWidget *widget, int section, QByteAr
 }
 
 void ImmediateDataWidgetMapper::clearMapping() {
+  // since we are keeping our own list to know what widgets were mapped with this mapper
+  // we have to clear it at the same time the QDataWidgetMapper clears its widget map
   widgetList.clear();
   QDataWidgetMapper::clearMapping();
 }
 
 void ImmediateDataWidgetMapper::setCurrentIndex(int index) {
+  // when the current index changes all the widgets have to be reloaded from the model
+  // we do not want them to signal property changes to us while they are reloading
+  // so we have to block their signals until the current index is changed and then
+  // unblock them afterwards if there were not blocked when this method entered
+  // NOTE: this probably is not threadsafe or reentrant, just like the rest of Qt
+  // and any other GUI framework, so take care when synchronizing it with other threads
   QList<QWidget *> wasBlocked;
   for (QWidget *widget : widgetList) {
     if (!widget->blockSignals(true)) {
