cc73504e11a4734392bdf14530b725014854c40b
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-08-31 02:08:41 -0400

    set da exe icon

--- qtplugins.cpp
+++ qtplugins.cpp
@@ -1,5 +1,10 @@
 // this source file is for importing QT plugins in
 // the static build of RadialGM that uses CMake
 #include <QtPlugin>
+
+Q_IMPORT_PLUGIN(QGifPlugin)
+Q_IMPORT_PLUGIN(QICOPlugin)
+Q_IMPORT_PLUGIN(QJpegPlugin)
+
 Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
 Q_IMPORT_PLUGIN(QWindowsVistaStylePlugin);
