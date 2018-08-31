90576c33bb0cc9d6b9309fc87b6f98133b01406d
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-08-31 15:14:16 -0400

    redirect gmk to stdout

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "MainWindow.h"
+#include "gmk.h"
 
 #include <QApplication>
+
+#include <iostream>
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
@@ -8,6 +11,10 @@ int main(int argc, char *argv[]) {
   a.setOrganizationDomain("enigma-dev.org");
   a.setApplicationName("RadialGM");
   a.setWindowIcon(QIcon(":/icon.ico"));
+
+  int test;
+  std::cerr << sizeof(test) << std::endl;
+  gmk::bind_output_streams(std::cout, std::cerr);
 
   MainWindow w(nullptr);
   if (argc > 1) {
