2354f37a8653c42c93d75be3856e99ad1215ac72
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-09-04 07:18:21 -0400

    remove unused includes

--- Editors/RoomEditor.cpp
+++ Editors/RoomEditor.cpp
@@ -1,10 +1,7 @@
 #include "RoomEditor.h"
 #include "ui_RoomEditor.h"
 
-#include <QDialog>
-#include <QFontDialog>
 #include <QGraphicsScene>
-#include <QLabel>
 
 RoomEditor::RoomEditor(ProtoModel* model, QWidget* parent) : BaseEditor(model, parent), ui(new Ui::RoomEditor) {
   ui->setupUi(this);
