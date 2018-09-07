e70348a149a622e2c0b4378b604e47820523799a
Robert Colton <robertbcolton@gmail.com>
2018-09-07 19:45:59 -0400

    Room Editor Prototype (#31)
    Add actual prototype room editor layout.

--- Editors/RoomEditor.cpp
+++ Editors/RoomEditor.cpp
@@ -1,8 +1,13 @@
 #include "RoomEditor.h"
 #include "ui_RoomEditor.h"
 
+#include <QGraphicsScene>
+
 RoomEditor::RoomEditor(ProtoModel* model, QWidget* parent) : BaseEditor(model, parent), ui(new Ui::RoomEditor) {
   ui->setupUi(this);
+
+  QGraphicsScene* scene = new QGraphicsScene(this);
+  ui->graphicsView->setScene(scene);
 }
 
 RoomEditor::~RoomEditor() { delete ui; }
