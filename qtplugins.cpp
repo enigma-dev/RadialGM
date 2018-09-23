1740ec9879624e1b522c6d3b4f1d6dc93663f485
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-09-23 16:46:25 -0400

    hrm

--- qtplugins.cpp
+++ qtplugins.cpp
@@ -2,13 +2,9 @@
 // the static build of RadialGM that uses CMake
 #include <QtPlugin>
 
-namespace RGM {
-
 Q_IMPORT_PLUGIN(QGifPlugin)
 Q_IMPORT_PLUGIN(QICOPlugin)
 Q_IMPORT_PLUGIN(QJpegPlugin)
 
-Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+//Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
 Q_IMPORT_PLUGIN(QWindowsVistaStylePlugin);
-
-}
