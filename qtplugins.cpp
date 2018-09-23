4e9f61202a8adcb139b0a0f5ac7afdd36c43ddd1
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-09-23 17:12:47 -0400

    bling

--- qtplugins.cpp
+++ qtplugins.cpp
@@ -6,5 +6,5 @@ Q_IMPORT_PLUGIN(QGifPlugin)
 Q_IMPORT_PLUGIN(QICOPlugin)
 Q_IMPORT_PLUGIN(QJpegPlugin)
 
-//Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
 Q_IMPORT_PLUGIN(QWindowsVistaStylePlugin);
