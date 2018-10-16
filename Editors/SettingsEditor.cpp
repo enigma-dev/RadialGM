3463c9ba41af7c68a821b77cb169535d1972f1dd
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-10-16 09:56:51 -0400

    add extension settings page and page switching

--- Editors/SettingsEditor.cpp
+++ Editors/SettingsEditor.cpp
@@ -17,22 +17,58 @@ SettingsEditor::SettingsEditor(ProtoModel* model, QWidget* parent)
   QPushButton* discardButton = ui->buttonBox->button(QDialogButtonBox::Discard);
   discardButton->setIcon(QIcon(":/actions/cancel.png"));
 
-  const QMap<QString, QComboBox*> systemUIMap = {
+  treePageMap = {{QString("ENIGMA/API"), ui->apiPage}, {QString("ENIGMA/Extensions"), ui->extensionsPage}};
+
+  const QMap<QString, QWidget*> systemUIMap = {
       {QString("Audio"), ui->audioCombo},          {QString("Platform"), ui->platformCombo},
       {QString("Graphics"), ui->graphicsCombo},    {QString("Widget"), ui->widgetsCombo},
       {QString("Collision"), ui->collisionsCombo}, {QString("Compilers"), ui->compilerCombo},
-      {QString("Network"), ui->networkingCombo},
+      {QString("Network"), ui->networkingCombo},   {QString("Extensions"), ui->extensionsList},
   };
   foreach (auto system, MainWindow::systemCache) {
     const QString systemName = QString::fromStdString(system.name());
     auto it = systemUIMap.find(systemName);
     if (it == systemUIMap.end()) continue;
+    auto widget = it.value();
+    const QString className = widget->metaObject()->className();
+    QListWidget* listWidget = nullptr;
+    QComboBox* combo = nullptr;
+    if (className == "QListWidget") {
+      listWidget = static_cast<QListWidget*>(widget);
+    } else if (className == "QComboBox") {
+      combo = static_cast<QComboBox*>(widget);
+    }
     foreach (auto subsystem, system.subsystems()) {
       const QString subsystemName = QString::fromStdString(subsystem.name());
-      auto combo = it.value();
-      combo->addItem(subsystemName);
+      const QString subsystemDesc = QString::fromStdString(subsystem.description());
+      if (combo)
+        combo->addItem(subsystemName);
+      else if (listWidget) {
+        auto item = new QListWidgetItem(subsystemName, listWidget);
+        item->setFlags(item->flags() | Qt::ItemFlag::ItemIsUserCheckable);
+        item->setCheckState(Qt::Unchecked);
+        item->setToolTip(subsystemDesc);
+      }
     }
   }
 }
 
 SettingsEditor::~SettingsEditor() { delete ui; }
+
+void SettingsEditor::on_treeWidget_currentItemChanged(QTreeWidgetItem* current, QTreeWidgetItem* /*previous*/) {
+  QWidget* widget = ui->emptyPage;
+  auto item = current;
+  if (item) {
+    QString path = item->text(0);
+    while (item->parent()) {
+      item = item->parent();
+      path.prepend(item->text(0) + "/");
+    }
+    auto it = treePageMap.find(path);
+    if (it != treePageMap.end()) {
+      widget = it.value();
+    }
+  }
+
+  ui->stackedWidget->setCurrentWidget(widget);
+}
--- Editors/SettingsEditor.h
+++ Editors/SettingsEditor.h
@@ -3,6 +3,8 @@
 
 #include "BaseEditor.h"
 
+#include <QTreeWidgetItem>
+
 namespace Ui {
 class SettingsEditor;
 }
@@ -14,8 +16,12 @@ class SettingsEditor : public BaseEditor {
   explicit SettingsEditor(ProtoModel* model, QWidget* parent);
   ~SettingsEditor();
 
+ private slots:
+  void on_treeWidget_currentItemChanged(QTreeWidgetItem* current, QTreeWidgetItem* previous);
+
  private:
   Ui::SettingsEditor* ui;
+  QMap<QString, QWidget*> treePageMap;
 };
 
 #endif  // SETTINGSEDITOR_H
