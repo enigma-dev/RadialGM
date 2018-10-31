a1efab0188be39b1a8a2ffe9eec39a0b954a1615
U-BEDROOM-DESKTOP\Owner <robertbcolton@gmail.com>
2018-10-30 21:16:46 -0400

    patch grid logic for half-pixel alignment efficiency

--- Widgets/AssetView.cpp
+++ Widgets/AssetView.cpp
@@ -14,21 +14,16 @@ const qreal& AssetView::GetZoom() const { return zoom; }
 void AssetView::paintGrid(QPainter& painter, int width, int height, int gridHorSpacing, int gridVertSpacing,
                           int gridHorOff, int gridVertOff, int gridWidth, int gridHeight) {
   painter.setCompositionMode(QPainter::RasterOp_SourceXorDestination);
-  painter.setPen(QColor(0xff, 0xff, 0xff));
+  painter.setPen(QColor(Qt::white));
 
   if (gridHorSpacing != 0 || gridVertSpacing != 0) {
-    for (int x = gridHorOff; x < width; x += gridWidth + gridHorSpacing) {
-      for (int y = gridVertOff; y < height; y += gridHeight + gridVertSpacing) {
+    for (int x = gridHorOff; x < width; x += gridWidth + gridHorSpacing + 1) {
+      for (int y = gridVertOff; y < height; y += gridHeight + gridVertSpacing + 1) {
         painter.drawRect(x, y, gridWidth, gridHeight);
       }
     }
-  }
-
-  if (gridHorSpacing == 0) {
+  } else {
     for (int x = gridHorOff; x <= width; x += gridWidth) painter.drawLine(x, 0, x, height);
-  }
-
-  if (gridVertSpacing == 0) {
     for (int y = gridVertOff; y <= height; y += gridHeight) painter.drawLine(0, y, width, y);
   }
 }
