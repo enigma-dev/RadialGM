94da7a06fea7fb1299df55fac1fe2f2a01b84163
Goombert <robertbcolton@gmail.com>
2020-03-24 22:20:50 -0400

    Fix head again!

--- Plugins/ServerPlugin.h
+++ Plugins/ServerPlugin.h
@@ -4,7 +4,7 @@
 #include "RGMPlugin.h"
 
 #ifndef _WIN32_WINNT
-	#define _WIN32_WINNT 0x0600  // at least windows vista required for grpc
+  #define _WIN32_WINNT 0x0600  // at least windows vista required for grpc
 #endif
 
 #include "server.grpc.pb.h"
@@ -30,70 +30,70 @@ using CompileMode = CompileRequest_CompileMode;
 enum AsyncState { DISCONNECTED = 0, READ = 1, WRITE = 2, CONNECT = 3, WRITES_DONE = 4, FINISH = 5 };
 
 class CallData : public QObject {
-	Q_OBJECT
+  Q_OBJECT
 
  public:
-	AsyncState state = DISCONNECTED;
-	Status status;
-	ClientContext context;
-	virtual ~CallData();
-	virtual void start() {}
-	virtual void operator()(const Status& status) = 0;
-	virtual void finish() {}
+  AsyncState state = DISCONNECTED;
+  Status status;
+  ClientContext context;
+  virtual ~CallData();
+  virtual void start() {}
+  virtual void operator()(const Status& status) = 0;
+  virtual void finish() {}
 
  signals:
-	void CompileStatusChanged(bool finished = false);
-	void LogOutput(const QString& output);
+  void CompileStatusChanged(bool finished = false);
+  void LogOutput(const QString& output);
 };
 
 class CompilerClient : public QObject {
-	Q_OBJECT
+  Q_OBJECT
 
  public:
-	explicit CompilerClient(std::shared_ptr<Channel> channel, MainWindow& mainWindow);
-	~CompilerClient() override;
-	void CompileBuffer(Game* game, CompileMode mode, std::string name);
-	void CompileBuffer(Game* game, CompileMode mode);
-	void GetResources();
-	void GetSystems();
-	void GetOutput();
-	void SetDefinitions(std::string code, std::string yaml);
-	void SetCurrentConfig(const resources::Settings& settings);
-	void SyntaxCheck();
+  explicit CompilerClient(std::shared_ptr<Channel> channel, MainWindow& mainWindow);
+  ~CompilerClient() override;
+  void CompileBuffer(Game* game, CompileMode mode, std::string name);
+  void CompileBuffer(Game* game, CompileMode mode);
+  void GetResources();
+  void GetSystems();
+  void GetOutput();
+  void SetDefinitions(std::string code, std::string yaml);
+  void SetCurrentConfig(const resources::Settings& settings);
+  void SyntaxCheck();
 
  signals:
-	void CompileStatusChanged(bool finished = false);
-	void LogOutput(const QString& output);
+  void CompileStatusChanged(bool finished = false);
+  void LogOutput(const QString& output);
 
  public slots:
-	void UpdateLoop();
+  void UpdateLoop();
 
  private:
-	CompletionQueue cq;
+  CompletionQueue cq;
 
-	template <typename T>
-	T* ScheduleTask();
+  template <typename T>
+  T* ScheduleTask();
 
-	std::unique_ptr<Compiler::Stub> stub;
-	MainWindow& mainWindow;
+  std::unique_ptr<Compiler::Stub> stub;
+  MainWindow& mainWindow;
 };
 
 class ServerPlugin : public RGMPlugin {
-	Q_OBJECT
+  Q_OBJECT
 
  public:
-	ServerPlugin(MainWindow& mainWindow);
-	~ServerPlugin() override;
+  ServerPlugin(MainWindow& mainWindow);
+  ~ServerPlugin() override;
 
  public slots:
-	void Run() override;
-	void Debug() override;
-	void CreateExecutable() override;
-	void SetCurrentConfig(const buffers::resources::Settings& settings) override;
+  void Run() override;
+  void Debug() override;
+  void CreateExecutable() override;
+  void SetCurrentConfig(const buffers::resources::Settings& settings) override;
 
  private:
-	QProcess* process;
-	CompilerClient* compilerClient;
+  QProcess* process;
+  CompilerClient* compilerClient;
 };
 
 #endif  // PLUGINSERVER_H
