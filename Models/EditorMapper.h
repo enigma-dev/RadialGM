bd377bd672279b4b2d54a273451a3678bbd28e39
Goombert <robertbcolton@gmail.com>
2020-08-13 15:37:55 -0400

    cleanup documentation and includes

--- Models/EditorMapper.h
+++ Models/EditorMapper.h
@@ -5,28 +5,17 @@
 
 #include <QObject>
 #include <QStack>
-#include <QHash>
-#include <QModelIndex>
-#include <QPersistentModelIndex>
 #include <QAbstractItemView>
-
-#include <google/protobuf/message.h>
-#include <google/protobuf/descriptor.h>
-
-using namespace google::protobuf;
 
 class MessageModel;
 class BaseEditor;
 
 /**
  * @brief The EditorMapper class used to map widgets into the model.
- * This class provides the data binding functionality allowing not
- * only single indexes into the model to map multiple widgets, but for
- * widgets to be grouped together by context. In other words, this
- * class supports 1:M relationships between QObject properties,
- * including widgets, and model indexes. The organization of the
- * internal source model and protocol buffer is abstracted behind the
- * compile-time generated constants and field numbers of resources.
+ * This class encapsulates the hierarchical data binding needed by
+ * the various editors. The organization of the internal source model
+ * and protocol buffer is abstracted behind the compile-time generated
+ * constants and field numbers of resources.
  */
 // NOTE: There is intentionally no API to remove individual mappings
 // in this class so that your tiny brain doesn't butcher what is
@@ -47,9 +36,9 @@ class EditorMapper : public QObject {
   using MapGroup = ModelMapper*;
 
   /**
-   * @brief mapField Maps a proto field in the model to an object property.
+   * @brief mapField Maps a proto field in the model to a widget property.
    * @param fieldNumber The compile-time generated field number constant.
-   * @param object The Qt object or widget we want to map.
+   * @param object The Qt widget we want to map.
    * @param property The property name of the object we want to map. The
    *                 default, an empty string, will instead look up the
    *                 USER property on the object, which is the one QWidgets
@@ -74,20 +63,20 @@ class EditorMapper : public QObject {
    */
   void clear();
   /**
-   * @brief pushField Moves current index into a repeated field.
+   * @brief pushField Moves current index into a field.
    *
-   * This can be used to map messages within a repeated field and easily
-   * remap the entire group later on. It's particular useful for master-detail
+   * This can be used to map messages within a repeated field and easily remap
+   * the entire group later on. It's particularly useful for master-detail
    * oriented views like the room editor's background settings which change
    * when the selected background changes.
    *
-   * @param fieldNumber The compile-time constant of the repeated field number.
+   * @param fieldNumber The compile-time constant of the field number.
    * @param index The index of the repeated field value, 0 by default.
    * @return MapGroup An index which you can later use to remap the fields.
    */
   MapGroup pushField(int fieldNumber, int index = 0);
   /**
-   * @brief pushResource Moves current index into the resource field.
+   * @brief pushAsset Moves current index into the resource field.
    *
    * Like mapName this is also a convenience function that keeps editors
    * loosely coupled from the structure of the tree node buffer. It's a
@@ -97,6 +86,16 @@ class EditorMapper : public QObject {
    * popResource, because you can just call this instead!
    */
   void pushAsset();
+  /**
+   * @brief pushView Moves current index into a field view.
+   *
+   * This can be used to create master-detail mappings in the editors.
+   * The view's selection change will automatically remap all of the
+   * fields that are mapped after this call.
+   *
+   * @param fieldNumber The compile-time constant of the field number.
+   * @param view The Qt view to map to the field.
+   */
   void pushView(int fieldNumber, QAbstractItemView *view);
   /**
    * @brief popField Moves current index back to the previous parent.
@@ -109,6 +108,11 @@ class EditorMapper : public QObject {
    * to the root as they initially were.
    */
   void popRoot();
+  /**
+   * @brief load Reverts all of the widgets in the group to the model values.
+   * @param group A specific group of mapped widgets to load. Can be null,
+   *              meaning load all mapped groups.
+   */
   void load(MapGroup group = nullptr);
 };
 
