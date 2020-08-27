b4b24da5ce32d6336234009da8b8d5e9f87e79e8
Goombert <robertbcolton@gmail.com>
2020-08-27 11:47:46 -0400

    add font combo and size spinner

--- Editors/InformationEditor.cpp
+++ Editors/InformationEditor.cpp
@@ -1,11 +1,20 @@
 #include "InformationEditor.h"
 #include "ui_InformationEditor.h"
 
+#include <QFontComboBox>
+#include <QSpinBox>
+
 InformationEditor::InformationEditor(QWidget *parent) :
   QWidget(parent),
   ui(new Ui::InformationEditor)
 {
   ui->setupUi(this);
+
+  auto fontCombo = new QFontComboBox();
+  ui->mainToolBar->insertWidget(ui->actionBold, fontCombo);
+  auto fontSpinner = new QSpinBox();
+  ui->mainToolBar->insertWidget(ui->actionBold, fontSpinner);
+  ui->mainToolBar->insertSeparator(ui->actionBold);
 }
 
 InformationEditor::~InformationEditor()
