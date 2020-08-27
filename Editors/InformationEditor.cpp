ca210d9cb70ddb32ff926f52f539ea84790a0b83
Goombert <robertbcolton@gmail.com>
2020-08-27 12:36:03 -0400

    add some more format functionality

--- Editors/InformationEditor.cpp
+++ Editors/InformationEditor.cpp
@@ -15,6 +15,24 @@ InformationEditor::InformationEditor(QWidget *parent) :
   auto fontSpinner = new QSpinBox();
   ui->mainToolBar->insertWidget(ui->actionBold, fontSpinner);
   ui->mainToolBar->insertSeparator(ui->actionBold);
+
+  connect(ui->actionBold, &QAction::triggered, [&](bool checked){
+    ui->textEdit->setFontWeight(checked ? QFont::Bold : QFont::Normal);
+  });
+  connect(ui->actionBulletedList, &QAction::triggered, [&](bool checked){
+    //TODO: Finish me
+  });
+  connect(ui->actionAlignGroup, &QActionGroup::triggered, [&](QAction *a){
+    if (a == ui->actionAlignLeft)
+        ui->textEdit->setAlignment(Qt::AlignLeft | Qt::AlignAbsolute);
+    else if (a == ui->actionAlignCenter)
+        ui->textEdit->setAlignment(Qt::AlignHCenter);
+    else if (a == ui->actionAlignRight)
+        ui->textEdit->setAlignment(Qt::AlignRight | Qt::AlignAbsolute);
+  });
+  connect(fontCombo, &QComboBox::textActivated, ui->textEdit, &QTextEdit::setFontFamily);
+  connect(fontSpinner, QOverload<int>::of(&QSpinBox::valueChanged), ui->textEdit,
+          &QTextEdit::setFontPointSize);
 }
 
 InformationEditor::~InformationEditor()
