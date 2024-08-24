5ec507b1d8ba82d763ad9e09f9ae9d93f8bff98e
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2024-08-24 15:54:16 +0300

    fixed emake server error and multiple improvements
    Signed-off-by: Saif Kandil <74428638+k0T0z@users.noreply.github.com>

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -42,6 +42,7 @@ QList<buffers::SystemType> MainWindow::systemCache;
 MainWindow *MainWindow::_instance = nullptr;
 ResourceModelMap *MainWindow::resourceMap = nullptr;
 TreeModel *MainWindow::treeModel = nullptr;
+MessageModel *MainWindow::protoModel = nullptr;
 std::unique_ptr<EventData> MainWindow::_event_data;
 
 static QTextEdit *diagnosticTextEdit = nullptr;
@@ -168,15 +169,14 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), _ui(new Ui::MainW
   settingsButton->setToolButtonStyle(Qt::ToolButtonStyle::ToolButtonTextBesideIcon);
   _ui->actionSettings->setMenu(_ui->menuChangeGameSettings);
 
+  /////////////////////////////
+  // Create the server plugin
+  /////////////////////////////
+
   RGMPlugin *pluginServer = new ServerPlugin(*this);
   auto outputTextBrowser = this->_ui->outputTextBrowser;
   connect(pluginServer, &RGMPlugin::LogOutput, outputTextBrowser, &QTextBrowser::append);
-  connect(pluginServer, &RGMPlugin::CompileStatusChanged, [=](bool finished) {
-    _ui->outputDockWidget->show();
-    _ui->actionRun->setEnabled(finished);
-    _ui->actionDebug->setEnabled(finished);
-    _ui->actionCreateExecutable->setEnabled(finished);
-  });
+  connect(pluginServer, &RGMPlugin::CompileStatusChanged, this, &MainWindow::on_compileStatus_changed);
   connect(this, &MainWindow::CurrentConfigChanged, pluginServer, &RGMPlugin::SetCurrentConfig);
   connect(_ui->actionRun, &QAction::triggered, pluginServer, &RGMPlugin::Run);
   connect(_ui->actionDebug, &QAction::triggered, pluginServer, &RGMPlugin::Debug);
@@ -186,6 +186,11 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), _ui(new Ui::MainW
 }
 
 MainWindow::~MainWindow() {
+  if (protoModel) delete protoModel;
+  if (treeModel) delete treeModel;
+  if (resourceMap) delete resourceMap;
+  if (toggleDiagnosticsAction) delete toggleDiagnosticsAction;
+  // if (this->pluginServer) delete this->pluginServer;
   diagnosticTextEdit = nullptr;
   delete _ui;
 }
@@ -416,13 +421,14 @@ void MainWindow::openProject(std::unique_ptr<buffers::Project> openedProject) {
   treeConf.SetMessagePassthrough<buffers::TreeNode::Folder>();
   treeConf.DisableOneofReassignment<buffers::TreeNode>();
 
-  delete resourceMap;
+  if (resourceMap) delete resourceMap;
   resourceMap = new ResourceModelMap(this);
 
-  auto pm = new MessageModel(ProtoModel::NonProtoParent{this}, _project->mutable_game()->mutable_root());
+  if (protoModel) delete protoModel;
+  protoModel = new MessageModel(ProtoModel::NonProtoParent{this}, _project->mutable_game()->mutable_root());
 
   // Connect methods to auto update fields with extensions
-  connect(pm, &ProtoModel::ModelConstructed, [](ProtoModel *model) {
+  connect(protoModel, &ProtoModel::ModelConstructed, [](ProtoModel *model) {
     PrimitiveModel *primitive_model = model->TryCastAsPrimitiveModel();
     if (primitive_model) {
       const FieldDescriptor *const field = primitive_model->GetFieldDescriptor();
@@ -437,13 +443,14 @@ void MainWindow::openProject(std::unique_ptr<buffers::Project> openedProject) {
     }
   });
 
-  pm->RebuildSubModels();
+  protoModel->RebuildSubModels();
 
-  pm->SetDisplayConfig(msgConf);
+  protoModel->SetDisplayConfig(msgConf);
 
-  resourceMap->TreeChanged(pm);
-  delete treeModel;
-  treeModel = new TreeModel(pm, nullptr, treeConf);
+  resourceMap->TreeChanged(protoModel);
+
+  if (treeModel) delete treeModel;
+  treeModel = new TreeModel(protoModel, nullptr, treeConf);
 
   _ui->treeView->setModel(treeModel);
   connect(treeModel, &TreeModel::ItemRenamed, resourceMap,
@@ -451,7 +458,7 @@ void MainWindow::openProject(std::unique_ptr<buffers::Project> openedProject) {
   connect(treeModel, &TreeModel::TreeChanged, resourceMap, &ResourceModelMap::TreeChanged);
   connect(treeModel, &TreeModel::ItemRemoved, resourceMap, &ResourceModelMap::ResourceRemoved,
           Qt::DirectConnection);
-  connect(pm, &ProtoModel::dataChanged, resourceMap, &ResourceModelMap::dataChanged,
+  connect(protoModel, &ProtoModel::dataChanged, resourceMap, &ResourceModelMap::dataChanged,
           Qt::DirectConnection);
   connect(treeModel, &TreeModel::ModelAboutToBeDeleted, this, &MainWindow::ResourceModelDeleted,
           Qt::DirectConnection);
@@ -716,4 +723,11 @@ void MainWindow::on_actionSortByName_triggered() {
 
 void MainWindow::on_treeView_customContextMenuRequested(const QPoint &pos) {
   _ui->menuEdit->exec(_ui->treeView->mapToGlobal(pos));
+}
+
+void MainWindow::on_compileStatus_changed(bool finished) {
+  _ui->outputDockWidget->show();
+  _ui->actionRun->setEnabled(finished);
+  _ui->actionDebug->setEnabled(finished);
+  _ui->actionCreateExecutable->setEnabled(finished);
 }
